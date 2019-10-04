574e569dc85b7718be0245a80f7ea3abd0027d3e
hybrid-syntax <abb.2005@gmail.com>
2019-10-04 19:41:28 +0330

    Commenting ReadFile & adding new dataset

--- ReadFile.py
+++ ReadFile.py
@@ -1,18 +1,25 @@
+#Import libraries
 import csv
-
 
 class CSVtoDictConverter:
     def open(self, filename):
         dictionary = dict()
         with open(filename, "r") as file:
+            # Read CSV file
             reader = csv.reader(file)
+
+            # Convert CSV file into transactional table
             for entry in reader:
+                # Reading columns
                 username = entry[0]
                 artist = entry[1]
                 rating = float(entry[2])
-                if username in dictionary:
-                    dictionary[username][artist] = rating
-                else:
-                    dictionary[username] = {artist: rating}
+                
+                # Create a new entry for user
+                if username not in dictionary:
+                    dictionary[username]=dict()
+                
+                # Adding the artist with rating to current user's list
+                dictionary[username][artist] = rating
 
         return dictionary
